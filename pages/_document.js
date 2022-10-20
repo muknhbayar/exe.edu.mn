e7ee050f185950c3dbce79edfec40d209eb09c99
Munkhbayar <mgl0814@gmail.com>
2022-10-20 12:17:34 +0800

    script_connect

--- pages/_document.js
+++ pages/_document.js
@@ -9,10 +9,10 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet"/>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css" />
-        <script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"async></script>
+        <Script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"async/>
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"></link>
-        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js" async></script>
-        <script src="/assets/js/scripts.js"async></script>
+        <Script src="https://unpkg.com/aos@2.3.1/dist/aos.js" async/>
+        <Script src="/assets/js/scripts.js"async/>
         
       </Head>
       <body>
